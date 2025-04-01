08182a3a55d12a41fb18ef39413141135a23602d
el1dor <is927861@gmail.com>
2025-04-01 18:13:21 +0300

    6.2 Домашнее задание Практическое применение после доработки

---
+++ app/src/main/java/com/example/correctedlibrary/ItemViewHolder.kt
@@ -0,0 +1,34 @@
+package com.example.correctedlibrary
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.correctedlibrary.databinding.LibraryItemBinding
+
+class ItemViewHolder(private val binding: LibraryItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Item) = with(binding)
+    {
+        item.imageRes?.let {
+            itemImage.setImageResource(it)
+        } ?: itemImage.setImageResource(R.drawable.book_image)
+        binding.itemName.text = item.itemName
+        binding.itemId.text = "ID: ${item.itemId}"
+        with(binding) {
+            if (item.isAvailable == false) {
+
+                itemId.alpha = 0.3f
+                itemName.alpha = 0.3f
+                cardView.elevation = 1f
+            } else {
+                itemId.alpha = 1f
+                itemName.alpha=1f
+                cardView.elevation = 10f
+            }
+
+        }
+
+
+        }
+
+    }
+
+
